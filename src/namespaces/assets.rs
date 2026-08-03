38096b7ac914571d932aa4e02959ae7e58306963
Johannes Pein <johannes.pein@xitaso.com>
2026-08-03 19:37:09 +0200

    refactor: hard-code registered namespaces

---
+++ src/namespaces/assets.rs
@@ -0,0 +1,13 @@
+pub(crate) const UNREGISTERED_PREFIX: &str = "x_";
+pub(crate) const FRAGMENT_DELIMITER: &str = "#";
+pub(crate) const SECTION_DELIMITER: &str = "/";
+
+pub(crate) const RESERVED_INVALID_NAMESPACES: &[&str] = &["invalid", "x_invalid"];
+
+pub(crate) const RESERVED_TEST_NAMESPACES: &[&str] = &["test", "x_test"];
+
+pub(crate) const RESERVED_EXAMPLE_NAMESPACES: &[&str] = &["example", "x_example"];
+
+/// All "registered" namespaces in the SSVC repository.
+pub const REGISTERED_NAMESPACES: &[&str] =
+    &["ssvc", "cvss", "cisa", "basic", "example", "test", "nist"];
