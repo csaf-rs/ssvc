2f34e9d309559e53b3eb536eee53ab1815150302
Johannes Pein <johannes.pein@xitaso.com>
2026-08-17 09:01:54 +0200

    feat: add selection list validation test cases for example and test namespace

--- tests/validation.rs
+++ tests/validation.rs
@@ -1,3 +1,4 @@
+use rstest::rstest;
 use ssvc::selection_list::SelectionList;
 use ssvc::{ValidationError, ValidationResult, validate_selection_list};
 
@@ -141,4 +142,63 @@ fn nist_800_30_passes() {
     let result = validate_selection_list(&selection_list, false);
 
     assert!(result.success);
+}
+
+#[test]
+fn example_namespace_passes() {
+    let json_data = r#"{
+              "schemaVersion": "2.0.0",
+              "selections": [
+                {
+                  "key": "E",
+                  "namespace": "example",
+                  "values": [
+                    {
+                      "key": "E",
+                      "name": "Example Value"
+                    }
+                  ],
+                  "version": "1.0.0"
+                }
+              ],
+              "timestamp": "2024-01-24T10:00:00.000Z"
+            }"#;
+
+    let selection_list: SelectionList =
+        serde_json::from_str(json_data).expect("SSVC SelectionList was invalid JSON");
+
+    let result = validate_selection_list(&selection_list, false);
+
+    assert!(result.success);
+}
+
+#[rstest]
+#[case::allow_test_true(true, true)]
+#[case::allow_test_false(false, false)]
+fn test_namespaces_passes_or_fails_depending_on_arg(#[case] allow_test: bool, #[case] expected: bool) {
+    let json_data = r#"{
+              "schemaVersion": "2.0.0",
+              "selections": [
+                {
+                  "key": "T",
+                  "namespace": "test",
+                  "values": [
+                    {
+                      "key": "T",
+                      "name": "Test Value"
+                    }
+                  ],
+                  "version": "1.0.0"
+                }
+              ],
+              "timestamp": "2024-01-24T10:00:00.000Z"
+            }"#;
+
+    let selection_list: SelectionList =
+        serde_json::from_str(json_data).expect("SSVC SelectionList was invalid JSON");
+
+    let result = validate_selection_list(&selection_list, allow_test);
+
+    assert_eq!(result.success, expected);
+
 }
