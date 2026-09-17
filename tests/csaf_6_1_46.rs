761f85773646c471fd1eb118063300892f13c18f
Johannes Pein <johannes.pein@xitaso.com>
2026-09-17 16:51:21 +0200

    style: fmt

--- tests/csaf_6_1_46.rs
+++ tests/csaf_6_1_46.rs
@@ -2,19 +2,24 @@ use ssvc::selection_list::SelectionList;
 
 /// Test based on CSAF 2.1 6-1-46-01
 #[test]
-fn test_6_1_46_01_deserialize_fails(){
+fn test_6_1_46_01_deserialize_fails() {
     let json_data = r#"{
   "schemaVersion": "2.0.0",
   "timestamp": "2024-01-24T10:00:00.000Z"
     }"#;
     let selection_list: Result<SelectionList, _> = serde_json::from_str(json_data);
     assert!(selection_list.is_err());
-    assert!(selection_list.unwrap_err().to_string().contains("missing field `selections`"));
+    assert!(
+        selection_list
+            .unwrap_err()
+            .to_string()
+            .contains("missing field `selections`")
+    );
 }
 
 /// Test based on CSAF 2.1 6-1-46-02
 #[test]
-fn test_6_1_46_02_deserialize_fails(){
+fn test_6_1_46_02_deserialize_fails() {
     let json_data = r#"{
   "schemaVersion": "2.0.0",
   "selections": [
@@ -36,12 +41,17 @@ fn test_6_1_46_02_deserialize_fails(){
 }"#;
     let selection_list: Result<SelectionList, _> = serde_json::from_str(json_data);
     assert!(selection_list.is_err());
-    assert!(selection_list.unwrap_err().to_string().contains("missing field `key`"));
+    assert!(
+        selection_list
+            .unwrap_err()
+            .to_string()
+            .contains("missing field `key`")
+    );
 }
 
 /// Test based on CSAF 2.1 6-1-46-11
 #[test]
-fn test_6_1_46_11_deserialize_ok(){
+fn test_6_1_46_11_deserialize_ok() {
     let json_data = r#"{
   "schemaVersion": "2.0.0",
   "selections": [
@@ -64,7 +74,7 @@ fn test_6_1_46_11_deserialize_ok(){
 
 /// Test based on CSAF 2.1 6-1-46-12
 #[test]
-fn test_6_1_46_12_deserialize_ok(){
+fn test_6_1_46_12_deserialize_ok() {
     let json_data = r#"{
   "schemaVersion": "2.0.0",
   "selections": [
@@ -89,4 +99,3 @@ fn test_6_1_46_12_deserialize_ok(){
     let selection_list: Result<SelectionList, _> = serde_json::from_str(json_data);
     assert!(selection_list.is_ok());
 }
-
